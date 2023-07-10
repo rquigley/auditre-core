bf3f3e09d5d51ed3b0bca936036ea247bad211bb
Ryan Quigley <ryan@simplehealth.com>
2023-07-10 18:27:27 -0400

    switch to v4 for next-auth

--- app/app/api/auth/[...nextauth]/route.ts
+++ app/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,42 @@
-export { GET, POST } from '@/auth';
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+//import prisma from "@/lib/prisma";
+import { compare } from "bcrypt";
 
-// import NextAuth, { NextAuthOptions } from 'next-auth';
-// import GithubProvider from 'next-auth/providers/github';
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials ?? {};
+        console.log("shit", email, password);
+        if (!email || !password) {
+          throw new Error("Missing username or password");
+        }
+        // const user = await prisma.user.findUnique({
+        //   where: {
+        //     email,
+        //   },
+        // });
+        const user = {
+          name: "haha",
+          email: "asds",
+          image: "asfdads",
+          password: "sdfsdf",
+        };
+        // if user doesn't exist or password doesn't match
+        if (!user || !(await compare(password, user.password))) {
+          throw new Error("Invalid username or password");
+        }
+        return user;
+      },
+    }),
+  ],
+};
 
-// export const authOptions: NextAuthOptions = {
-//   providers: [
-//     GithubProvider({
-//       clientId: process.env.OAUTH_CLIENT_KEY as string,
-//       clientSecret: process.env.OAUTH_CLIENT_SECRET as string,
-//     }),
-//   ],
-// };
+const handler = NextAuth(authOptions);
 
-// const handler = NextAuth(authOptions);
-
-// export { handler as GET, handler as POST };
+export { handler as GET, handler as POST };
--- app/app/login/page.tsx
+++ app/app/login/page.tsx
@@ -1,16 +1,31 @@
-import { auth } from '@/auth';
-import { LoginButton } from '@/components/login-button';
-import { redirect } from 'next/navigation';
+import Image from "next/image";
+import Form from "@/components/form";
+import Link from "next/link";
+import LoginForm from './login-form'
 
-export default async function SignInPage() {
-  const session = await auth();
-  // redirect to home if user is already logged in
-  if (session?.user) {
-    redirect('/');
-  }
+
+export default function Login() {
   return (
-    <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
-      <LoginButton />
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
+      <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
+        <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              priority
+              alt="Logo"
+              className="h-10 w-10 rounded-full"
+              width={20}
+              height={20}
+            />
+          </Link>
+          <h3 className="text-xl font-semibold">Sign In</h3>
+          <p className="text-sm text-gray-500">
+            Use your email and password to sign in
+          </p>
+        </div>
+        <LoginForm />
+      </div>
     </div>
   );
 }
