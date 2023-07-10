67c6427f063590df9772ad80de5251d42a8822ff
Ryan Quigley <ryan@simplehealth.com>
2023-07-10 18:28:07 -0400

    redundant config loading

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,15 +1,19 @@
-import { Pool } from "pg";
-import { Kysely, PostgresDialect, CamelCasePlugin } from "kysely";
-import { Database } from "@/types";
-console.log(process.env);
+import { Pool } from 'pg';
+import { Kysely, PostgresDialect, CamelCasePlugin } from 'kysely';
+import { Database } from '@/types';
+
 const dialect = new PostgresDialect({
-  pool: async () =>
-    new Pool({
+  pool: async () => {
+    if (!process.env.DB_DATABASE) {
+      throw new Error('DB_DATABASE not set');
+    }
+    return new Pool({
       database: process.env.DB_DATABASE,
       host: process.env.DB_HOSTNAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-    }),
+    });
+  },
 });
 
 export const db = new Kysely<Database>({
