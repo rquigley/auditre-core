da3c7773e04fbc1d227aae7690c35e924a8970f4
Ryan Quigley <ryan@simplehealth.com>
2023-07-10 18:28:44 -0400

    single-quote!

--- app/controllers/session.ts
+++ app/controllers/session.ts
@@ -1,46 +1,47 @@
-import { db } from "@/lib/db";
-import { SessionUpdate, Session, NewSession } from "@/types";
+import { db } from '@/lib/db';
+import { SessionUpdate, Session, NewSession } from '@/types';
 
-export async function create(user: NewSession): Promise<Session> {
+export function create(session: NewSession): Promise<Session> {
   return db
-    .insertInto("session")
-    .values(user)
+    .insertInto('session')
+    .values(session)
     .returningAll()
     .executeTakeFirstOrThrow();
 }
 
-export async function deleteBySessionToken(
-  sessionToken: string
-): Promise<number> {
+export function deleteBySessionToken(
+  sessionToken: string,
+): Promise<Session | undefined> {
   return db
-    .deleteFrom("session")
-    .where("sessionToken", "=", sessionToken)
+    .deleteFrom('session')
+    .where('sessionToken', '=', sessionToken)
     .returningAll()
     .executeTakeFirst();
 }
-export async function getById(id: number): Promise<Session> {
+
+export function getById(id: number): Promise<Session> {
   return db
-    .selectFrom("session")
-    .where("id", "=", id)
+    .selectFrom('session')
+    .where('id', '=', id)
     .selectAll()
     .executeTakeFirstOrThrow();
 }
 
-export async function getByUserId(userId: number): Promise<Session> {
+export function getByUserId(userId: number): Promise<Session> {
   return db
-    .selectFrom("session")
-    .where("userId", "=", userId)
+    .selectFrom('session')
+    .where('userId', '=', userId)
     .selectAll()
     .executeTakeFirstOrThrow();
 }
 
-export async function updateBySessionToken(
+export function updateBySessionToken(
   sessionToken: string,
-  updateWith: SessionUpdate
+  updateWith: SessionUpdate,
 ) {
   return db
-    .updateTable("session")
+    .updateTable('session')
     .set(updateWith)
-    .where("sessionToken", "=", sessionToken)
+    .where('sessionToken', '=', sessionToken)
     .execute();
 }
