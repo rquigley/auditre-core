1cd382058e803524ee3d3726d860baa599c52f9d
Ryan Quigley <ryan@simplehealth.com>
2023-07-10 18:29:34 -0400

    password generation util

---
+++ app/lib/util.ts
@@ -0,0 +1,20 @@
+export function generatePassword(
+  length: number = 8,
+  includeSymbols = true,
+): string {
+  let charset;
+  if (includeSymbols) {
+    charset =
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+  } else {
+    charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  }
+  let password = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+
+  return password;
+}
