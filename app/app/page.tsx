98656d114c08678791559f08f48e0d63f9aefbd8
Ryan Quigley <ryan@simplehealth.com>
2023-07-10 18:50:15 -0400

    working

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import Nav from './nav';
 import { Suspense } from 'react';
-import { auth } from '@/auth';
+//import { auth } from '@/auth';
+import { unstable_getServerSession } from 'next-auth/next';
+
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  const session = await auth();
+  //const session = await auth();
+  const session = await unstable_getServerSession();
 
   if (!session?.user) {
     redirect(`/login?next=/requests`);
--- app/controllers/user.ts
+++ app/controllers/user.ts
@@ -1,9 +1,9 @@
-import { db } from "@/lib/db";
-import { UserUpdate, User, NewUser } from "@/types";
+import { db } from '@/lib/db';
+import { UserUpdate, User, NewUser } from '@/types';
 
 export async function create(user: NewUser): Promise<User> {
   return db
-    .insertInto("user")
+    .insertInto('user')
     .values(user)
     .returningAll()
     .executeTakeFirstOrThrow();
@@ -11,33 +11,33 @@ export async function create(user: NewUser): Promise<User> {
 
 export async function getById(id: number): Promise<User> {
   return db
-    .selectFrom("user")
-    .where("id", "=", id)
+    .selectFrom('user')
+    .where('id', '=', id)
     .selectAll()
     .executeTakeFirstOrThrow();
 }
 
 export async function getByEmail(email: string): Promise<User | undefined> {
   return db
-    .selectFrom("user")
-    .where("email", "=", email)
+    .selectFrom('user')
+    .where('email', '=', email)
     .selectAll()
     .executeTakeFirst();
 }
 
 export async function getByAccountProviderAndProviderId(
   provider: string,
-  providerAccountId: string
+  providerAccountId: string,
 ): Promise<User | undefined> {
   return db
-    .selectFrom("user")
-    .innerJoin("account", "account.userId", "user.id")
-    .where("account.provider", "=", provider)
-    .where("account.providerAccountId", "=", providerAccountId)
-    .selectAll("user")
+    .selectFrom('user')
+    .innerJoin('account', 'account.userId', 'user.id')
+    .where('account.provider', '=', provider)
+    .where('account.providerAccountId', '=', providerAccountId)
+    .selectAll('user')
     .executeTakeFirst();
 }
 
 export async function update(id: number, updateWith: UserUpdate) {
-  return db.updateTable("user").set(updateWith).where("id", "=", id).execute();
+  return db.updateTable('user').set(updateWith).where('id', '=', id).execute();
 }
