6fd7ef4185f76ad69171e50ba8eaebf17037fd07
Ryan Quigley <ryan@simplehealth.com>
2023-07-16 15:52:34 -0400

    switch from unstable

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image';
 import Nav from './nav';
 import { Suspense } from 'react';
 //import { auth } from '@/auth';
-import { unstable_getServerSession } from 'next-auth/next';
+import { getServerSession } from 'next-auth/next';
 
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
   //const session = await auth();
-  const session = await unstable_getServerSession();
+  const session = await getServerSession();
 
   if (!session?.user) {
     redirect(`/login?next=/requests`);
