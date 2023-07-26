898659b6ebb2bb01007e8a1b48a7b0cbc907cb57
Ryan Quigley <rquigley@gmail.com>
2023-07-26 18:05:36 -0400

    sort imports

---
+++ app/prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+  // plugins: [require('@trivago/prettier-plugin-sort-imports')],
+  singleQuote: true,
+  // importOrder: ['^@/(.*)$', '^[./]'],
+  // importOrderSeparation: true,
+  // importOrderSortSpecifiers: true,
+};
