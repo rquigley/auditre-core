6b46ff0423b969b67f542a9f3ecaa97d51a9741b
Ryan Quigley <ryan@simplehealth.com>
2023-07-26 13:35:48 -0400

    improve notification for requested requests pt 2

--- app/app/audit/[audit]/request-row.tsx
+++ app/app/audit/[audit]/request-row.tsx
@@ -8,21 +8,14 @@ function StatusBadge({ status }: { status: string }) {
   switch (status) {
     case 'requested':
       return (
-        <span className="inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-gray-900 ring-1 ring-inset ring-gray-200">
-          <svg
-            className="h-1.5 w-1.5 fill-blue-500"
-            viewBox="0 0 6 6"
-            aria-hidden="true"
-          >
-            <circle cx={3} cy={3} r={3} />
-          </svg>
-          Requested
+        <span className="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+          Todo
         </span>
       );
     case 'complete':
       return (
-        <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-          Complete
+        <span className="inline-flex items-center rounded-full bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
+          Done
         </span>
       );
     case 'overdue':
