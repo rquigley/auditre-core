98a67ca4d0eec4383440ed0094a1a84c62515faa
Ryan Quigley <ryan@simplehealth.com>
2023-08-02 17:31:55 -0400

    add SQS and lambda test implementation

--- ops/lib/ops-stack.ts
+++ ops/lib/ops-stack.ts
@@ -1,10 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
 
 export class OpsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const queue = new sqs.Queue(this, 'sqs-queue-test-1', {
+      queueName: 'OurSQSQueue',
+    });
+
+    const lambdaFunction = new lambda.Function(this, 'Function', {
+      code: lambda.Code.fromAsset('src'),
+      handler: 'index.handler',
+      functionName: 'SqsMessageHandler',
+      runtime: lambda.Runtime.NODEJS_18_X,
+    });
+
+    const eventSource = new lambdaEventSources.SqsEventSource(queue);
+
+    lambdaFunction.addEventSource(eventSource);
 
     new s3.Bucket(this, 'Bah7Ogh9', {
       bucketName: 'com-auditrehq-org-files-dev',
@@ -23,7 +41,9 @@ export class OpsStack extends cdk.Stack {
           allowedHeaders: ['*'],
         },
       ],
-      //objectLockEnabled: true,
+      //objectLockEnabled: true, // should be true for prod
     });
+
+    //https://dev.to/aws-builders/how-to-trigger-an-aws-lambda-from-sqs-2lkc
   }
 }
