a7cc13abf4d758a4e6975f4e4a12cd058c45313d
Ryan Quigley <rquigley@gmail.com>
2023-08-04 14:15:25 -0400

    improve signing of s3 upload/download

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,14 +1,38 @@
 'use server';
 import { getPresignedUrl } from './aws';
 import { z } from 'zod';
+import { getByExternalId } from '@/controllers/request';
+import { getById } from '@/controllers/audit';
+import { getById as getOrgById } from '@/controllers/org';
+import { nanoid } from 'nanoid';
 
 const bucketSchema = z.string();
+const filenameSchema = z.string().min(4).max(128);
 
-export async function getPresignedUploadUrl(key: string) {
+export async function getPresignedUploadUrl({
+  filename: unsanitizedFilename,
+  requestExternalId,
+}: {
+  filename: string;
+  requestExternalId: string;
+}) {
+  const filename = filenameSchema.parse(unsanitizedFilename);
   const bucket = bucketSchema.parse(process.env.AWS_S3_BUCKET);
 
-  return await getPresignedUrl({
+  const request = await getByExternalId(requestExternalId);
+  const audit = await getById(request.auditId);
+  const org = await getOrgById(audit.orgId);
+
+  const key = `${org.externalId}/${audit.externalId}/${
+    request.externalId
+  }/${nanoid()}/${filename}`;
+  const url = await getPresignedUrl({
     key,
     bucket,
   });
+  return {
+    url,
+    key,
+    bucket,
+  };
 }
