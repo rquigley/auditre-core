8bdfb9f2d3e0a6daa9e37b07dcbaa2010c89a914
Ryan Quigley <ryan@simplehealth.com>
2023-08-08 13:58:21 -0400

    add ability to extract and save content from xlsx and pdf documents

---
+++ app/lib/ai.ts
@@ -0,0 +1,52 @@
+import { Configuration, OpenAIApi } from 'openai';
+import { z } from 'zod';
+
+const openaiConfig = z.string().min(3).max(255);
+
+export async function summarize(content: string) {
+  return await call([
+    { role: 'system', content: 'Summarize this in under 20 words' },
+    { role: 'user', content },
+  ]);
+}
+
+export async function askQuestion(question: string, content: string) {
+  return await call([
+    {
+      role: 'system',
+      content: question,
+    },
+    { role: 'user', content },
+  ]);
+}
+
+type Message = {
+  role: 'user' | 'system';
+  content: string;
+};
+async function call(messages: Message[]) {
+  const apiKey = openaiConfig.parse(process.env.OPENAI_API_KEY);
+  const configuration = new Configuration({
+    apiKey,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  let model;
+  // TODO: better estimate
+  if (JSON.stringify(messages).length > 4000) {
+    model = 'gpt-3.5-turbo-16k';
+  } else {
+    model = 'gpt-3.5-turbo';
+  }
+
+  const chatCompletion = await openai.createChatCompletion({
+    model,
+    // model: 'gpt-3.5-turbo-16k',
+    // messages: [{ role: 'user', content: 'Hello world' }],
+    messages,
+  });
+  //console.log(chatCompletion.data);
+  return chatCompletion.data.choices[0].message;
+  //console.log(chatCompletion.data.choices[0].message);
+}
--- app/next.config.js
+++ app/next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  // The webpack config below is to allow pdfjs to work. It otherwise kills the server
+  // saying canvas is not found.
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack },
+  ) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      //
+      canvas: false,
+      //encoding: false,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
