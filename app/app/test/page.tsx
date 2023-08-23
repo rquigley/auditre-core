c0aade9ae6a04e7a5e5c2760815ee29ea7b142b5
Ryan Quigley <ryan@simplehealth.com>
2023-08-23 13:20:50 -0400

    config

--- app/app/test/page.tsx
+++ app/app/test/page.tsx
@@ -1,4 +1,16 @@
 import { headers } from 'next/headers';
+import fs from 'fs';
+
+async function getEnvFileContents() {
+  let data = await fs.promises.readFile('.env.local', 'utf8');
+  data = data.replace(/OPENAI_API_KEY.*/g, '');
+  data = data.replace(/GOOGLE_CLIENT_ID.*/g, '');
+  data = data.replace(/GOOGLE_CLIENT_SECRET.*/g, '');
+  data = data.replace(/NEXTAUTH_SECRET.*/g, '');
+  data = data.replace(/DB_HOSTNAME.*/g, '');
+  data = data.replace(/DB_USER.*/g, '');
+  return data;
+}
 
 export default async function Home() {
   const headersList = headers();
@@ -17,7 +29,8 @@ export default async function Home() {
   return (
     <div>
       <pre>{JSON.stringify({ headers: headersList.entries() }, null, 2)}</pre>n
-      <pre>{JSON.stringify({ other }, null, 2)}</pre>n
+      <pre>{JSON.stringify({ other }, null, 2)}</pre>
+      <pre>{await getEnvFileContents()}</pre>
     </div>
   );
 }
