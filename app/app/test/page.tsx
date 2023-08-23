aafea7476c4cc3b242650dbfe820227cfaa2c95d
Ryan Quigley <rquigley@gmail.com>
2023-08-23 13:54:48 -0400

    improvements to debug

--- app/app/test/page.tsx
+++ app/app/test/page.tsx
@@ -1,22 +1,9 @@
 import { headers } from 'next/headers';
-import fs from 'fs';
-
-async function getEnvFileContents() {
-  let data = await fs.promises.readFile('.env.local', 'utf8');
-  data = data.replace(/OPENAI_API_KEY.*/g, '');
-  data = data.replace(/GOOGLE_CLIENT_ID.*/g, '');
-  data = data.replace(/GOOGLE_CLIENT_SECRET.*/g, '');
-  data = data.replace(/NEXTAUTH_SECRET.*/g, '');
-  data = data.replace(/DB_HOSTNAME.*/g, '');
-  data = data.replace(/DB_USER.*/g, '');
-  return data;
-}
 
 export default async function Home() {
   const headersList = headers();
-  const NEXTAUTH = process.env.NEXTAUTH_URL;
   const other = {
-    NEXTAUTH: process.env.NEXTAUTH_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     referer: headersList.get('referer'),
     host: headersList.get('host'),
     Host: headersList.get('Host'),
@@ -30,7 +17,6 @@ export default async function Home() {
     <div>
       <pre>{JSON.stringify({ headers: headersList.entries() }, null, 2)}</pre>n
       <pre>{JSON.stringify({ other }, null, 2)}</pre>
-      <pre>{await getEnvFileContents()}</pre>
     </div>
   );
 }
