bc3b37874a42262fc86142a44a763da1b1a9714c
Ryan Quigley <rquigley@gmail.com>
2023-08-24 17:54:54 -0400

    switch register user to invite

--- app/bin/register.ts
+++ app/bin/register.ts
@@ -1,9 +1,7 @@
 import { program } from 'commander';
 import prompts from 'prompts';
 import { loadEnvConfig } from '@next/env';
-
-import { create as createUser } from '@/controllers/user';
-import { create as createPassword } from '@/controllers/password';
+import { create as createInvitation } from '@/controllers/invitation';
 import { create as createOrg } from '@/controllers/org';
 import { db } from '@/lib/db';
 import type { OrgId } from '@/types';
@@ -11,20 +9,6 @@ import type { OrgId } from '@/types';
 const dev = process.env.NODE_ENV !== 'production';
 loadEnvConfig(process.cwd(), dev, { info: () => null, error: console.error });
 
-async function registerUser(orgId: OrgId, email: string, password?: string) {
-  const user = await createUser({
-    orgId,
-    email,
-  });
-
-  await createPassword({
-    userId: user.id,
-    value: password,
-  });
-  console.log(
-    `Created User with\nID: ${user.id}\nEmail: ${email}\nPassword: ${password}`,
-  );
-}
 program
   .command('org')
   .description('Creates an organization.')
@@ -39,31 +23,16 @@ program
 
 program
   .command('user')
-  .description('Creates a user with a given email.')
+  .description('Creates an invite for a given email.')
   .argument('<orgId>', 'Organization ID')
   .argument('<email>', 'user email')
-  .option('-P', 'Prompt for a password')
   .action(async (orgId, email, opts) => {
     let password;
 
-    if (opts.P) {
-      const resp = await prompts({
-        type: 'password',
-        name: 'value',
-        message: 'Password:',
-        validate: (value) => {
-          // TODO: Use a password validator
-          if (value.length < 4) {
-            return 'Invalid password';
-          } else {
-            return true;
-          }
-        },
-      });
-      password = resp.value;
-    }
-
-    await registerUser(orgId, email, password);
+    const invitation = await createInvitation({
+      orgId: orgId,
+      email,
+    });
     await db.destroy();
   });
 program.parse();
