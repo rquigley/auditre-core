f7c0d2f0371c9057e253d4205f50f8780b586ef4
Ryan Quigley <ryan@simplehealth.com>
2023-08-23 22:09:58 -0400

    test cookie

--- app/app/test/page.tsx
+++ app/app/test/page.tsx
@@ -1,6 +1,11 @@
-import { headers } from 'next/headers';
+import { headers, cookies } from 'next/headers';
 
 export default async function Home() {
+  async function testCookie() {
+    'use server';
+    cookies().set('yep', 'nope');
+  }
+
   const headersList = headers();
   const other = {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
@@ -8,7 +13,6 @@ export default async function Home() {
 
     referer: headersList.get('referer'),
     host: headersList.get('host'),
-    Host: headersList.get('Host'),
     Origin: headersList.get('Origin'),
     'x-forwarded-host': headersList.get('x-forwarded-host'),
     'x-forwarded-for': headersList.get('x-forwarded-for'),
@@ -18,6 +22,9 @@ export default async function Home() {
   return (
     <div>
       <pre>{JSON.stringify({ other }, null, 2)}</pre>
+      <form action={testCookie}>
+        <button type="submit">Add to Cart</button>
+      </form>
     </div>
   );
 }
