076f40a1c148d3427abf5a324ab09a9b971a4c1f
Ryan Quigley <rquigley@gmail.com>
2023-08-25 16:51:38 -0400

    allow db to be recreated without dropping the db, which will allow it to be run in AWS... STAGING, not prod ;)

--- app/bin/db-reset.js
+++ app/bin/db-reset.js
@@ -1,28 +1,59 @@
 const { Client } = require('pg');
 const { migrateToLatest } = require('./db-migrate');
 
-async function reCreateDb() {
+const database = process.env.DB_DATABASE;
+
+async function createDb() {
   const client = new Client({
     database: 'postgres',
     host: process.env.DB_HOSTNAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
   });
+  await client.connect();
+  await client.query(`CREATE DATABASE ${database}`);
+  await client.end();
+}
 
-  const database = process.env.DB_DATABASE;
+let db;
+async function resetDb() {
+  db = new Client({
+    database,
+    host: process.env.DB_HOSTNAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  });
 
   if (!database) {
     throw new Error('DB_DATABASE is not set');
   }
   console.log(`Recreating Database ${database}`);
-  await client.connect();
-  await client.query(`DROP DATABASE IF EXISTS ${database}`);
-  await client.query(`CREATE DATABASE ${database}`);
-  await client.end();
+  await db.connect();
+  const res = await db.query(`
+do $do$ declare
+  r record;
+begin
+  for r in (select tablename from pg_tables where schemaname = 'public') loop
+      execute 'drop table if exists ' || quote_ident(r.tablename) || ' cascade';
+  end loop;
+end $do$;
+`);
+  await db.end();
 }
 
 async function run() {
-  await reCreateDb();
+  try {
+    await resetDb();
+  } catch (err) {
+    await db.end();
+
+    if (err.code == '3D000') {
+      await createDb();
+      await resetDb();
+    } else {
+      throw err;
+    }
+  }
   await migrateToLatest();
 }
 
