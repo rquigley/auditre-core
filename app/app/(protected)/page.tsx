c2eb9afc98a62c4a838e1251414754b3ef33fa1f
Ryan Quigley <ryan@simplehealth.com>
2023-08-29 11:16:28 -0400

    add sketch for dashboard

--- app/app/(protected)/page.tsx
+++ app/app/(protected)/page.tsx
@@ -1,3 +1,11 @@
 export default async function Home() {
-  return null;
+  return (
+    <div>
+      <div>This is the dashboard</div>
+      <div>- timeline?</div>
+      <div>- deadlines?</div>
+      <div>- request progress?</div>
+      <div>- outstanding notifications for user?</div>
+    </div>
+  );
 }
