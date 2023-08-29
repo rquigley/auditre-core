bd47f1669d0c3b2cb4087c7d8cfad5bc580e5b69
Ryan Quigley <ryan@simplehealth.com>
2023-08-29 12:25:03 -0400

    clean up redirector

--- app/app/(protected)/redirector.tsx
+++ app/app/(protected)/redirector.tsx
@@ -1,8 +1,6 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { usePathname, redirect } from 'next/navigation';
-import { signIn, useSession } from 'next-auth/react';
-import { cookies } from 'next/headers';
 
 export default function Redirector({
   setPostAuthUrl,
@@ -13,7 +11,6 @@ export default function Redirector({
   const [redirectUrl, setRedirectUrl] = useState<string | null>(null);
 
   useEffect(() => {
-    console.log('shit', pathname);
     if (pathname) {
       setPostAuthUrl(pathname).then(() => {
         setRedirectUrl(pathname);
@@ -26,15 +23,4 @@ export default function Redirector({
     redirect('/login');
   }
   return null;
-  //const { status } = useSession();
-  const status = 'asd';
-  console.log({ status, pathname });
-  return (
-    <div>
-      <div>
-        <a href={`/login?url=${pathname}`}>login</a>
-      </div>
-      {JSON.stringify({ status, pathname })}
-    </div>
-  );
 }
