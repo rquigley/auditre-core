7c25fc9b9e99efe293c5a309841251e7e6f8ad0a
Ryan Quigley <ryan@simplehealth.com>
2023-08-29 12:39:03 -0400

    remove unused form types

--- app/app/(protected)/request/[request]/form-container.tsx
+++ app/app/(protected)/request/[request]/form-container.tsx
@@ -3,7 +3,7 @@ import { updateData } from '@/controllers/request';
 import { create as createDocument } from '@/controllers/document';
 import BasicForm from './basic-form';
 import { requestTypes } from '@/lib/request-types';
-import type { Request, User, Audit, S3File } from '@/types';
+import type { Request, User, Audit, S3File, ClientSafeRequest } from '@/types';
 import { clientSafe } from '@/lib/util';
 import { revalidatePath } from 'next/cache';
 
@@ -13,20 +13,7 @@ type Props = {
   audit: Audit;
 };
 
-const formComponents = {
-  BUSINESS_NAME: BasicForm,
-  BUSINESS_DESCRIPTION: BasicForm,
-  BUSINESS_MODEL: BasicForm,
-  MULTIPLE_BUSINESS_LINES: BasicForm,
-  USER_REQUESTED: BasicForm,
-} as const;
-
 export default async function FormContainer({ request, user, audit }: Props) {
-  // if (!formComponents.hasOwnProperty(request.type)) {
-  //   throw new Error(`Invalid request type ${request.type}`);
-  // }
-  // const FormCmp = formComponents[request.type];
-  const FormCmp = BasicForm;
   const requestConfig = requestTypes[request.type];
 
   async function saveData(data: z.infer<typeof formSchema>) {
@@ -34,18 +21,18 @@ export default async function FormContainer({ request, user, audit }: Props) {
     const formSchema = requestTypes[request.type].schema;
 
     for (const [key, field] of Object.entries(requestConfig.form)) {
+      if (!data.hasOwnProperty(key)) {
+        continue;
+      }
       if (
         field.input === 'fileupload' &&
         //@ts-ignore
-
         data[key] &&
         // data[key].key indicates that a new file has been uploaded
         //@ts-ignore
-
         data[key].key
       ) {
         //@ts-ignore
-
         const file: S3File = data[key];
         const doc = await createDocument({
           key: file.key,
@@ -79,9 +66,8 @@ export default async function FormContainer({ request, user, audit }: Props) {
   }
 
   return (
-    <FormCmp
-      //@ts-ignore
-      request={clientSafe(request)}
+    <BasicForm
+      request={clientSafe(request) as ClientSafeRequest}
       data={request.data}
       //@ts-ignore
       saveData={saveData}
