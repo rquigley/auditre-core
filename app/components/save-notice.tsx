82f9ca079f0c4a83e07118abc4269e652d264f9e
Ryan Quigley <rquigley@gmail.com>
2023-08-30 22:00:49 -0400

    add save-notice

---
+++ app/components/save-notice.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { CheckCircleIcon } from '@heroicons/react/20/solid';
+
+export function SaveNotice() {
+  const [hidden, setHidden] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setHidden(true);
+    }, 5000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  if (hidden) return null;
+  return (
+    <div>
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <CheckCircleIcon
+            className="h-5 w-5 text-green-400"
+            aria-hidden="true"
+          />
+        </div>
+        <div className="ml-3">
+          <p className="text-sm font-medium text-green-800">
+            Your changes have been saved
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
