c85c0f13bdcf7acd91a654be2ee9512343507f52
Ryan Quigley <rquigley@gmail.com>
2023-08-31 14:05:34 -0400

    clean up dashboard

--- app/app/(protected)/page.tsx
+++ app/app/(protected)/page.tsx
@@ -1,11 +1,15 @@
+import Header from '@/components/header';
+
 export default async function Home() {
   return (
-    <div>
-      <div>This is the dashboard</div>
-      <div>- timeline?</div>
-      <div>- deadlines?</div>
-      <div>- request progress?</div>
-      <div>- outstanding notifications for user?</div>
-    </div>
+    <>
+      <Header title="Dashboard" />
+      <div>
+        <div>- timeline?</div>
+        <div>- deadlines?</div>
+        <div>- request progress?</div>
+        <div>- outstanding notifications for user?</div>
+      </div>
+    </>
   );
 }
