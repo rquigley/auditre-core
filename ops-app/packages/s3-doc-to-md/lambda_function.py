da015a832dce5a752bb8e0daa54368199216bdb6
Ryan Quigley <rquigley@gmail.com>
2023-09-04 10:52:10 -0400

    convert xlsx files to csv

--- ops-app/packages/s3-doc-to-md/lambda_function.py
+++ ops-app/packages/s3-doc-to-md/lambda_function.py
@@ -3,11 +3,11 @@ import tempfile
 import os
 from docx import Document
 from pypdf import PdfReader
+from openpyxl import load_workbook
+import csv
 import boto3
 from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration
 import sentry_sdk
-
-# https://stackoverflow.com/questions/56818579/unable-to-import-lxml-etree-on-aws-lambda
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -37,38 +37,49 @@ def handler(event, context):
 
     # Determine the file type from the file extension
     file_type = key.split('.')[-1]
+    # file_name =  os.path.basename(key)
 
-    # Create a temporary directory to store the downloaded file
     with tempfile.TemporaryDirectory() as tmp_dir:
-        download_path = os.path.join(tmp_dir, 'downloaded_file')
-        upload_path = os.path.join(tmp_dir, 'converted_file.md')
+        download_path = os.path.join(tmp_dir, f"downloaded_file.{file_type}")
+        upload_path = os.path.join(tmp_dir, 'extracted_file')
 
-        # Download the file from S3
         s3.download_file(bucket, key, download_path)
 
-        # Convert the file to Markdown
         if file_type == 'pdf':
             convert_pdf_to_md(download_path, upload_path)
         elif file_type in ['doc', 'docx']:
             convert_docx_to_md(download_path, upload_path)
+        elif file_type == 'xlsx':
+            convert_xlsx_to_csv(download_path, upload_path)
 
-        # Upload the Markdown file back to S3
-        new_key = f"{key}.md"
+        new_key = f"{key}.extracted"
         s3.upload_file(upload_path, bucket, new_key)
 
 
-def convert_pdf_to_md(pdf_path, md_path):
-    with open(pdf_path, 'rb') as f:
+def convert_pdf_to_md(read_path, extracted_path):
+    with open(read_path, 'rb') as f:
         pdf_reader = PdfReader(f)
-        with open(md_path, 'w') as md_file:
+        with open(extracted_path, 'w') as extracted_file:
             for page in pdf_reader.pages:
-                md_file.write(page.extract_text())
-                md_file.write('\n\n')
+                extracted_file.write(page.extract_text())
+                extracted_file.write('\n\n')
 
 
-def convert_docx_to_md(docx_path, md_path):
-    doc = Document(docx_path)
-    with open(md_path, 'w') as md_file:
+def convert_docx_to_md(read_path, extracted_path):
+    doc = Document(read_path)
+    with open(extracted_path, 'w') as extracted_file:
         for para in doc.paragraphs:
-            md_file.write(para.text)
-            md_file.write('\n\n')
+            extracted_file.write(para.text)
+            extracted_file.write('\n\n')
+
+
+def convert_xlsx_to_csv(read_path, extracted_path):
+    wb = load_workbook(read_path)
+    ws = wb.active
+    with open(extracted_path, 'w') as extracted_file:
+        writer = csv.writer(extracted_file)
+        for row in ws.rows:
+            writer.writerow([cell.value for cell in row])
+
+
+# convert_xlsx_to_csv('./test.xlsx', './test.csv')
