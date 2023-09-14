3304c5aa08fa9d11a4345a8c1a5464d1214e3d70
Ryan Quigley <ryan@simplehealth.com>
2023-09-14 13:07:22 -0400

    Pull due-date from requests. move status to left

--- app/app/(protected)/audit/[audit]/page.tsx
+++ app/app/(protected)/audit/[audit]/page.tsx
@@ -43,26 +43,17 @@ export default async function AuditPage({
                 <tr>
                   <th
                     scope="col"
-                    className="relative py-3.5 pl-3 pr-4 sm:pr-0"
-                  ></th>
+                    className="w-20 pl-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Status
+                  </th>
                   <th
                     scope="col"
                     className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
                   >
                     Request
                   </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                  >
-                    Status
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                  >
-                    Due Date
-                  </th>
+
                   <th
                     scope="col"
                     className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
