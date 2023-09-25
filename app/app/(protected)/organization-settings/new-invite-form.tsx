d28b9c89ae8a663b792095210b247d0b834e72e6
Ryan Quigley <rquigley@gmail.com>
2023-09-25 16:52:41 -0400

    add tools for org management

---
+++ app/app/(protected)/organization-settings/new-invite-form.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { classNames } from '@/lib/util';
+import {
+  // @ts-ignore
+  experimental_useFormState as useFormState,
+  experimental_useFormStatus as useFormStatus,
+} from 'react-dom';
+
+const initialState = {
+  message: null,
+};
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      type="submit"
+      aria-disabled={pending}
+      className={classNames(
+        pending
+          ? 'bg-gray-400'
+          : 'bg-sky-700 hover:bg-sky-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-700',
+        'mt-6 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm',
+      )}
+    >
+      Send Invite
+    </button>
+  );
+}
+
+export default function NewInviteForm({
+  createInvite,
+}: {
+  createInvite: (
+    prevState: any,
+    formData: FormData,
+  ) => Promise<{ message: string }>;
+}) {
+  const [state, formAction] = useFormState(createInvite, initialState);
+
+  return (
+    <form action={formAction}>
+      <div className="sm:col-span-4">
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          New invite email
+        </label>
+        <div className="mt-2">
+          <input
+            id="email"
+            name="email"
+            type="text"
+            className="block w-70 rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+      <SubmitButton />
+      <p aria-live="polite" className="sr-only" role="status">
+        {state?.message}
+      </p>
+    </form>
+  );
+}
--- app/app/(protected)/organization-settings/page.tsx
+++ app/app/(protected)/organization-settings/page.tsx
@@ -1,13 +1,164 @@
-import { getCurrent } from '@/controllers/session-user';
+import InviteSubmenu from './invite-submenu';
+import NewInviteForm from './new-invite-form';
+import Datetime from '@/components/datetime';
 import Header from '@/components/header';
+import {
+  deleteInvitation,
+  getAllByOrgId as getInvitations,
+} from '@/controllers/invitation';
+import { create as createInvitation } from '@/controllers/invitation';
+import { getCurrent } from '@/controllers/session-user';
+import { getAllByOrgId as getUsers } from '@/controllers/user';
+import { classNames } from '@/lib/util';
+import type { Invitation, User } from '@/types';
+import { ChevronRightIcon } from '@heroicons/react/20/solid';
+import { revalidatePath } from 'next/cache';
+import Link from 'next/link';
+import { z } from 'zod';
+
+const createInviteSchema = z.object({
+  email: z.string().email(),
+});
+const deleteInviteSchema = z.object({
+  inviteId: z.string(),
+});
 
 export default async function OrganizationSettingsPage() {
   const user = await getCurrent();
+  const users = await getUsers(user.orgId);
+  const invitations = await getInvitations(user.orgId);
+
+  async function createInvite(prevState: any, formData: FormData) {
+    'use server';
+    try {
+      const data = createInviteSchema.parse({
+        email: formData.get('email'),
+      });
+
+      await createInvitation({
+        email: data.email,
+        orgId: user.orgId,
+      });
+      revalidatePath('/organization-settings');
+      return { message: `Invited ${data.email}` };
+    } catch (error) {
+      return { message: 'Failed to create invite' };
+    }
+  }
 
   return (
     <>
       <Header title="Organization Settings" />
-      <div className="mt-8 flow-root">todo</div>
+      <div className="mt-8 flow-root">Users</div>
+      <Users users={users} />
+
+      <div className="mt-8 flow-root">Invitations</div>
+      <Invitations invitations={invitations} />
+      <NewInviteForm createInvite={createInvite} />
     </>
+  );
+}
+
+function Users({ users }: { users: User[] }) {
+  return (
+    <ul role="list" className="divide-y divide-gray-100">
+      {users.map((user) => (
+        <li
+          key={user.email}
+          className="relative flex justify-between gap-x-6 py-5"
+        >
+          <div className="flex min-w-0 gap-x-4">
+            {user.image && (
+              <img
+                className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                src={user.image}
+                alt=""
+              />
+            )}
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm font-semibold leading-6 text-gray-900">
+                <Link href={`/organization-settings/user/${user.id}`}>
+                  <span className="absolute inset-x-0 -top-px bottom-0" />
+                  {user.name}
+                </Link>
+              </p>
+              <p className="mt-1 flex text-xs leading-5 text-gray-500">
+                <span className="relative truncate">{user.email}</span>
+              </p>
+            </div>
+          </div>
+          <div className="flex shrink-0 items-center gap-x-4">
+            <div className="hidden sm:flex sm:flex-col sm:items-end">
+              {/* <p className="text-sm leading-6 text-gray-900">{user.role}</p>
+              {user.lastSeen ? (
+                <p className="mt-1 text-xs leading-5 text-gray-500">
+                  Last seen{' '}
+                  <time dateTime={user.lastSeenDateTime}>{user.lastSeen}</time>
+                </p>
+              ) : null} */}
+            </div>
+            <ChevronRightIcon
+              className="h-5 w-5 flex-none text-gray-400"
+              aria-hidden="true"
+            />
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+function Invitations({ invitations }: { invitations: Invitation[] }) {
+  async function deleteInvite(prevState: any, formData: FormData) {
+    'use server';
+    try {
+      const data = deleteInviteSchema.parse({
+        inviteId: formData.get('inviteId'),
+      });
+
+      await deleteInvitation(data.inviteId);
+
+      revalidatePath('/organization-settings');
+      return { message: `Deleted` };
+    } catch (error) {
+      return { message: 'Failed to delete invite' };
+    }
+  }
+
+  const now = Date.now();
+  return (
+    <ul role="list" className="divide-y divide-gray-100">
+      {invitations.map((user) => {
+        const isExpired = user.expiresAt.getTime() - now < 0;
+        return (
+          <li
+            key={user.id}
+            className="relative flex justify-between gap-x-6 py-5"
+          >
+            <div className="flex min-w-0 gap-x-4">
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-gray-900">
+                  <span className="absolute inset-x-0 -top-px bottom-0" />
+                  {user.email}
+                </p>
+                <p
+                  className={classNames(
+                    isExpired ? 'text-red-700' : '',
+                    'mt-1 flex text-xs leading-5 text-gray-500',
+                  )}
+                >
+                  <span className="mr-1">
+                    {isExpired ? 'Expired ' : 'Expires '}
+                  </span>
+                  <Datetime dateTime={user.expiresAt} />
+                </p>
+              </div>
+            </div>
+            <div className="flex shrink-0 items-center gap-x-4">
+              <InviteSubmenu invitation={user} deleteInvite={deleteInvite} />
+            </div>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
