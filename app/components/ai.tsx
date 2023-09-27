70df828bdd6003d644dfdf33043fed5af4b0b672
Ryan Quigley <ryan@simplehealth.com>
2023-09-27 15:08:07 -0400

    ai form cleanup

--- app/components/ai.tsx
+++ app/components/ai.tsx
@@ -1,9 +1,12 @@
-import type { Document } from '@/types';
-import { getAllByDocumentId } from '@/controllers/document-query';
-import AIForm from './ai-form';
-import { askQuestion } from '@/controllers/document-query';
 import { revalidatePath } from 'next/cache';
+
+import CopyToClipboard from '@/components/copy-to-clipboard';
+import { askQuestion, getAllByDocumentId } from '@/controllers/document-query';
 import { OpenAIModel } from '@/types';
+import AIForm from './ai-form';
+import Datetime from './datetime';
+
+import type { Document } from '@/types';
 
 export default async function AI({ document }: { document: Document }) {
   const queries = await getAllByDocumentId(document.id);
@@ -23,21 +26,43 @@ export default async function AI({ document }: { document: Document }) {
   return (
     <div className="">
       <div className="text-med leading-6 text-gray-500 mt-4">AI</div>
-      <div className="text-xs">
+      <div className="text-xs ">
         {queries.map((query) => (
-          <div key={query.id} className="flex flex-row">
-            <div className="flex flex-col my-2">
-              <div className="text-xs text-gray-500">
-                <span className="font-bold">{query.query}</span>
-              </div>
-              <div className="text-xs text-gray-500">
-                ({query.model}, Tokens: {query.usage?.totalTokens} prompt,{' '}
-                {query.usage?.completionTokens} completion, Time:{' '}
-                {(query.usage?.timeMs / 1000).toFixed(2)}s)
-                <pre className="w-180 whitespace-normal">
-                  {query.result?.content}
-                </pre>
-              </div>
+          <div
+            key={query.id}
+            className="my-4 p-4 border border-1 border-slate-300 rounded-md "
+          >
+            <div className="mb-2">
+              Identifier: {query.identifier}
+              <br />
+              Model: {query.model}
+              <br />
+              Tokens: {query.usage?.totalTokens} prompt,{' '}
+              {query.usage?.completionTokens} completion
+              <br />
+              Time: {(query.usage?.timeMs / 1000).toFixed(2)}s<br />
+              Created: <Datetime dateTime={query.createdAt} />
+            </div>
+
+            <div className="text-xs text-gray-500">
+              <span className="font-bold" id={`query-${query.id}`}>
+                Q: {nl2br(query.query)}
+              </span>
+            </div>
+            <div className="text-xs text-gray-500 mt-2">
+              <pre
+                className="w-180 whitespace-normal"
+                id={`result-${query.id}`}
+              >
+                A: {nl2br(query.result?.content)}
+              </pre>
+
+              {/* <CopyToClipboard elementId={`query-${query.id}`}>
+                  Copy query
+                </CopyToClipboard>
+                <CopyToClipboard elementId={`result-${query.id}`}>
+                  Copy result
+                </CopyToClipboard> */}
             </div>
           </div>
         ))}
@@ -45,4 +70,15 @@ export default async function AI({ document }: { document: Document }) {
       <AIForm saveData={saveData} />
     </div>
   );
+}
+
+function nl2br(str: string) {
+  return str.split('\n').map((item, key) => {
+    return (
+      <span key={key}>
+        {item}
+        <br />
+      </span>
+    );
+  });
 }
