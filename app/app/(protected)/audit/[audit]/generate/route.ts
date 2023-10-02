4338e3ada5d2be1d89509142b566130e7ad056c2
Ryan Quigley <ryan@simplehealth.com>
2023-10-02 15:33:42 -0400

    add button to generate financial statement

---
+++ app/app/(protected)/audit/[audit]/generate/route.ts
@@ -0,0 +1,33 @@
+import { notFound, redirect } from 'next/navigation';
+import { NextResponse } from 'next/server';
+
+import { getById as getAuditById } from '@/controllers/audit';
+import { generate } from '@/controllers/audit-output';
+import { getCurrent } from '@/controllers/session-user';
+
+const { Packer } = require('docx');
+
+export async function GET(
+  req: Request,
+  {
+    params: { audit: auditId },
+  }: {
+    params: { audit: string };
+  },
+) {
+  const user = await getCurrent();
+  const audit = await getAuditById(auditId);
+  if (audit.orgId !== user.orgId) {
+    return notFound();
+  }
+
+  const { document, documentName } = await generate(auditId);
+
+  const b64string = await Packer.toBase64String(document);
+
+  return new NextResponse(Buffer.from(b64string, 'base64'), {
+    headers: {
+      'content-disposition': `attachment; filename=${documentName}`,
+    },
+  });
+}
