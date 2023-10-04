cf51663b62cf7daa3e8eeeb8d6ce27ef0783dd26
Ryan Quigley <rquigley@gmail.com>
2023-10-04 17:18:52 -0400

    use validated key

--- app/lib/ai.ts
+++ app/lib/ai.ts
@@ -38,7 +38,7 @@ async function call(
 ) {
   const apiKey = openaiConfig.parse(process.env.OPENAI_API_KEY);
   const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey,
   });
 
   if (!model) {
