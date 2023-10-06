0070194c0d2c5fc95d73fd96b02a2fd3901d8116
Ryan Quigley <ryan@simplehealth.com>
2023-10-06 15:23:22 -0400

    import sorting

---
+++ ops-marketing/prettier.config.js
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  singleQuote: true,
+  importOrder: [
+    '<BUILTIN_MODULES>',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/(.*)$',
+    '^[./]',
+    '',
+    '<TYPES>',
+  ],
+  importOrderTypeScriptVersion: '5.0.0',
+};
