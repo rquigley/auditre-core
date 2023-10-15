ea88cb3269dfa59f33a7431aaca4be652c388a7a
Ryan Quigley <rquigley@gmail.com>
2023-10-15 12:27:45 -0400

    export NoSuchKey on inability to find object in aws

--- app/lib/aws.ts
+++ app/lib/aws.ts
@@ -2,6 +2,7 @@ import { writeFile } from 'node:fs/promises';
 import { extname } from 'path';
 import {
   GetObjectCommand,
+  NoSuchKey,
   PutObjectCommand,
   S3Client,
 } from '@aws-sdk/client-s3';
@@ -10,6 +11,8 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { z } from 'zod';
 
 import type { Readable } from 'stream';
+
+export { NoSuchKey };
 
 const region = 'us-east-2';
 
