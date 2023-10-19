adf075aa633f97b645fe3af780c5de94584636ab
Ryan Quigley <ryan@simplehealth.com>
2023-10-19 13:16:02 -0400

    audit settings/delete

--- app/app/(protected)/audit/[audit]/settings/page.tsx
+++ app/app/(protected)/audit/[audit]/settings/page.tsx
@@ -1,10 +1,10 @@
 import { notFound } from 'next/navigation';
-import { Fragment } from 'react';
 
 import { getById } from '@/controllers/audit';
-import { getAllByAuditId } from '@/controllers/request';
 import { getCurrent } from '@/controllers/session-user';
 import { AuditHeader } from '../audit-header';
+import { DeleteAuditButton } from './delete-audit-button';
+import { SettingsForm } from './settings-form';
 
 export default async function AuditPage({
   params: { audit: id },
@@ -24,13 +24,11 @@ export default async function AuditPage({
 
       <div className="mt-8 flow-root">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            Name: {audit.name}
-            <br />
-            Year: {audit.year}
-            <br />
-            Delete
-            <br />
+          <div className="inline-block py-2 align-middle sm:px-6 lg:px-8">
+            <SettingsForm audit={audit} />
+            <div className="mt-4">
+              <DeleteAuditButton auditId={audit.id} />
+            </div>
           </div>
         </div>
       </div>
