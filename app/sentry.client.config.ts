b543cd7c3e4f0bf73954720e90c1f229c4be0de3
Ryan Quigley <rquigley@gmail.com>
2023-11-02 10:06:28 -0400

    disable sentry performance montitoring and replay

--- app/sentry.client.config.ts
+++ app/sentry.client.config.ts
@@ -9,16 +9,16 @@ Sentry.init({
   enabled: process.env.NODE_ENV !== 'development',
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
-  replaysOnErrorSampleRate: 1.0,
+  replaysOnErrorSampleRate: 0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
