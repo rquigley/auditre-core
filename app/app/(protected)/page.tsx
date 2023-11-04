2fbc9905b326601dc13e54549c619729821f41eb
Ryan Quigley <ryan@simplehealth.com>
2023-11-04 14:49:17 -0400

    cleanup home

--- app/app/(protected)/page.tsx
+++ app/app/(protected)/page.tsx
@@ -1,14 +1,14 @@
-import Header from '@/components/header';
+// import Header from '@/components/header';
 
 export default async function Home() {
   return (
     <>
-      <Header title="Dashboard" />
+      {/* <Header title="Home" /> */}
       <div>
-        <div>- timeline?</div>
-        <div>- deadlines?</div>
-        <div>- request progress?</div>
-        <div>- outstanding notifications for user?</div>
+        <div>- timeline</div>
+        <div>- deadlines</div>
+        <div>- request progress</div>
+        <div>- outstanding notifications for user</div>
       </div>
     </>
   );
