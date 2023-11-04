5a65988a967371697117ecb7195d27daeff2a092
Ryan Quigley <ryan@simplehealth.com>
2023-11-04 12:58:32 -0400

    cache status render

--- app/app/(protected)/audit/[audit]/row.tsx
+++ app/app/(protected)/audit/[audit]/row.tsx
@@ -1,51 +1,53 @@
 'use client';
 
 import Link from 'next/link';
-import { Suspense } from 'react';
+import { Suspense, useMemo } from 'react';
 
 import { Await } from '@/components/await';
 
 import type { ClientSafeRequest } from '@/controllers/request';
 
 function StatusBadge({ status }: { status: string }) {
-  switch (status) {
-    case 'loading':
-      return (
-        <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium">
-          &nbsp;
-        </span>
-      );
-    case 'todo':
-      return (
-        <span className="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-          Todo
-        </span>
-      );
-    case 'started':
-      return (
-        <span className="inline-flex items-center rounded-full bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
-          Started
-        </span>
-      );
-    case 'complete':
-      return (
-        <span className="inline-flex items-center rounded-full bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
-          Done
-        </span>
-      );
-    case 'overdue':
-      return (
-        <span className="inline-flex items-center rounded-md bg-red-400/10 px-2 py-1 text-xs font-medium text-red-400 ring-1 ring-inset ring-red-400/20">
-          Overdue
-        </span>
-      );
-    default:
-      return (
-        <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-          {status}
-        </span>
-      );
-  }
+  return useMemo(() => {
+    switch (status) {
+      case 'loading':
+        return (
+          <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium">
+            &nbsp;
+          </span>
+        );
+      case 'todo':
+        return (
+          <span className="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+            Todo
+          </span>
+        );
+      case 'started':
+        return (
+          <span className="inline-flex items-center rounded-full bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+            Started
+          </span>
+        );
+      case 'complete':
+        return (
+          <span className="inline-flex items-center rounded-full bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
+            Done
+          </span>
+        );
+      case 'overdue':
+        return (
+          <span className="inline-flex items-center rounded-md bg-red-400/10 px-2 py-1 text-xs font-medium text-red-400 ring-1 ring-inset ring-red-400/20">
+            Overdue
+          </span>
+        );
+      default:
+        return (
+          <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+            {status}
+          </span>
+        );
+    }
+  }, [status]);
 }
 
 export default function Row({
