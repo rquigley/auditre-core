656d6912f721d26d8011f3fba41ed44fce9e79dc
Ryan Quigley <ryan@auditre.co>
2023-12-02 17:22:42 -0500

    excel improvements

--- app/lib/finance.ts
+++ app/lib/finance.ts
@@ -14,3 +14,28 @@ export function groupFixedAccountsByCategories(
   }
   return result;
 }
+
+export function groupAccountTypes(
+  types: Record<string, string>,
+): Record<string, Record<string, string>> {
+  const grouped: Record<string, Record<string, any>> = {
+    Asset: {},
+    Liability: {},
+    Equity: {},
+    'Income Statement': {},
+  };
+
+  for (const key in types) {
+    if (key.startsWith('ASSET_')) {
+      grouped.Asset[key] = types[key];
+    } else if (key.startsWith('LIABILITY_')) {
+      grouped.Liability[key] = types[key];
+    } else if (key.startsWith('EQUITY_')) {
+      grouped.Equity[key] = types[key];
+    } else if (key.startsWith('INCOME_STATEMENT_')) {
+      grouped['Income Statement'][key] = types[key];
+    }
+  }
+
+  return grouped;
+}
