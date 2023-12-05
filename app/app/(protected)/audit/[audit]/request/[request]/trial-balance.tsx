93811b0a4c13de0451ef372399cff2319941757f
Ryan Quigley <ryan@auditre.co>
2023-12-05 16:54:40 -0500

    use monospaced font for finance info

--- app/app/(protected)/audit/[audit]/request/[request]/trial-balance.tsx
+++ app/app/(protected)/audit/[audit]/request/[request]/trial-balance.tsx
@@ -1,10 +1,19 @@
+import { Inconsolata } from 'next/font/google';
+
 import { SecondaryButton } from '@/components/button';
-import { getAllAccountBalancesByAuditId } from '@/controllers/account-mapping';
+import {
+  accountTypes,
+  getAllAccountBalancesByAuditId,
+} from '@/controllers/account-mapping';
 import { extractTrialBalance } from '@/lib/actions';
 import { ppCurrency } from '@/lib/util';
 
 import type { AuditId } from '@/types';
 
+export const financeFont = Inconsolata({
+  subsets: ['latin'],
+  display: 'swap',
+});
 export async function TrialBalance({ auditId }: { auditId: AuditId }) {
   const accountMapping = await getAllAccountBalancesByAuditId(auditId);
 
@@ -17,13 +26,7 @@ export async function TrialBalance({ auditId }: { auditId: AuditId }) {
               scope="col"
               className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
             >
-              Account Id
-            </th>
-            <th
-              scope="col"
-              className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
-            >
-              Account name
+              Account
             </th>
             <th
               scope="col"
@@ -33,13 +36,13 @@ export async function TrialBalance({ auditId }: { auditId: AuditId }) {
             </th>
             <th
               scope="col"
-              className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+              className="whitespace-nowrap min-w-min px-2 py-3.5 text-right text-sm font-semibold text-gray-900"
             >
               Credit
             </th>
             <th
               scope="col"
-              className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+              className="whitespace-nowrap min-w-min px-2 py-3.5 text-right text-sm font-semibold text-gray-900"
             >
               Debit
             </th>
@@ -59,24 +62,28 @@ export async function TrialBalance({ auditId }: { auditId: AuditId }) {
             accountMapping.map((am) => (
               <tr key={am.id}>
                 <td className="w-30 py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">
-                  {am.accountNumber || '-'}
+                  {am.accountNumber || ''}
+                  {am.accountNumber && am.accountName ? ' - ' : ''}
+                  {am.accountName || ''}
                 </td>
-                <td className="w-30 py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">
-                  {am.accountName || '-'}
-                </td>
-                <td className="w-30 px-2 py-2 text-sm text-gray-900">
-                  {am.mappedToAccountName ? (
-                    am.mappedToAccountName
+                <td className="w-30 px-2 py-2 text-xs text-gray-900">
+                  {am.accountType === 'UNKNOWN' ? (
+                    <span className="text-red-600">Unknown</span>
                   ) : (
-                    <span className="text-red-600">Could not map</span>
+                    accountTypes[am.accountType]
                   )}
                 </td>
-                <td className="w-30 px-2 py-2 text-sm font-medium text-gray-900">
+                <td
+                  className={`w-30 px-2 py-2 text-sm font-medium text-gray-900 text-right ${financeFont}`}
+                >
+                  {' '}
                   {am.credit > 0
                     ? ppCurrency(parseFloat(am.credit as unknown as string))
                     : '-'}
                 </td>
-                <td className="w-30 px-2 py-2 text-sm font-medium text-gray-900">
+                <td
+                  className={`w-30 px-2 py-2 text-sm font-medium text-gray-900 text-right ${financeFont}`}
+                >
                   {am.debit > 0
                     ? ppCurrency(parseFloat(am.debit as unknown as string))
                     : '-'}
