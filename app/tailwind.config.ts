a54992c02251cc4f7246d24404bc96a223c59744
Ryan Quigley <ryan@auditre.co>
2023-12-13 14:49:54 -0500

    switch tailwind config to ts

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.tsx',
+    './app/**/*.tsx',
     './controllers/output/react.tsx',
   ],
   theme: {
@@ -11,3 +12,5 @@ module.exports = {
   },
   plugins: [],
 };
+
+export default config;
