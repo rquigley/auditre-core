df6201306d63d0a746a583722f3cf6afa6e9179b
Ryan Quigley <ryan@auditre.co>
2023-12-14 14:15:08 -0500

    start removal of chart of accounts

--- app/lib/document-ai-questions.ts
+++ app/lib/document-ai-questions.ts
@@ -108,64 +108,6 @@ export const documentAiQuestions: Partial<
         'What is the jurisdiction of incorporation? Answer only with the jurisdiction',
     },
   },
-  CHART_OF_ACCOUNTS: {
-    columnMappings: {
-      preProcess: (val: string) => head(val, 10),
-      question: dedent`
-            In this CSV content extract the following column properties:
-
-            1. accountIdColumnIdx
-            Which column contains the account IDs? The columns are often named 'ID', 'Account No.' or 'Code'. It is usually the first or second column and values for rows likely contain numbers.
-            If you can't find an account ID column, set accountIdColumnIdx to -1
-
-            2. accountNameColumnIdx
-            Which column contains the account names? The columns are often  labeled 'Account', 'Account Name', 'Name'. It is usually the first or second column and values for rows likely contain letters.
-            If you can't find an account name column, set accountNameColumnIdx to -1
-
-            3. otherColIdxs
-            Which column ids contain other information? Don't include the columns you already identified in 1 and 2. If you can't find any other columns, set otherColIdxs to an empty array.
-
-            Return JSON with these four properties. Examples:
-            1. """Input CSV
-            *Code,*Name,*Type,*Tax Code,Description,Dashboard,Expense Claims,Enable Payments,Balance\r                                                                                                                                                                                                                                                                                                                                                                                                                  +
-            1200,Accounts Receivable,Accounts Receivable,Tax Exempt (0%),Outstanding invoices the company has issued out to the client but has not yet received in cash at balance date.,No,No,No,\r                                                                                                                                                                                                                                                                                                                    +
-            1300,Prepayments,Current Asset,Tax Exempt (0%),An expenditure tha...
-            """
-
-            """Output JSON
-            {
-              "accountIdColumnIdx": 0,
-              "accountNameColumnIdx": 1,
-              "otherColIdxs": [2, 3, 4, 5, 6, 7, 8, 9]
-            }
-            """
-
-            2. """Input CSV
-            Account /Parent Account,Account Number,Account Type,Account Sub Type,Fresbhook Balance
-            Cash,1000,Asset,Cash & Bank,$0.00
-            Accounts Receivable,1200,Asset,Current Asset,$0.00...
-            """
-
-            """Output JSON
-            {
-              "accountIdColumnIdx": 1,
-              "accountNameColumnIdx": 0,
-              "otherColIdxs": [2, 3, 4]
-            }
-            """
-            `,
-      respondInJSON: true,
-      validate: z.object({
-        accountIdColumnIdx: z.number().min(-1).max(10),
-        accountNameColumnIdx: z.number().min(-1).max(10),
-        otherColIdxs: z.array(z.number().min(0).max(100)),
-      }),
-    },
-    // {
-    //   id: 'accountMapping',
-    //   fn: extractChartOfAccountsMapping,
-    // },
-  },
   TRIAL_BALANCE: {
     trialBalanceDate: {
       label: 'Date of trial balance export',
