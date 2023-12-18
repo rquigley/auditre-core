6f2b043e28e185b213c74f87cd37fc8a723b5739
Ryan Quigley <ryan@auditre.co>
2023-12-18 13:17:54 -0500

    mini to micro icons

--- app/components/table.tsx
+++ app/components/table.tsx
@@ -14,13 +14,13 @@ export function Arrow({ isActive }: { isActive: boolean }) {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
+        viewBox="0 0 16 16"
         fill="currentColor"
-        className="w-5 h-5"
+        className="w-4 h-4"
       >
         <path
           fillRule="evenodd"
-          d="M10 5a.75.75 0 01.75.75v6.638l1.96-2.158a.75.75 0 111.08 1.04l-3.25 3.5a.75.75 0 01-1.08 0l-3.25-3.5a.75.75 0 111.08-1.04l1.96 2.158V5.75A.75.75 0 0110 5z"
+          d="M8 2a.75.75 0 0 1 .75.75v8.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.22 3.22V2.75A.75.75 0 0 1 8 2Z"
           clipRule="evenodd"
         />
       </svg>
