dd6f8baccd6ae8313bb6f290dba9d616e80f4431
Ryan Quigley <ryan@auditre.co>
2024-02-02 11:27:50 -0500

    update GPT-4 model

--- app/components/ai-form.tsx
+++ app/components/ai-form.tsx
@@ -30,7 +30,7 @@ export default function AI({
 
     defaultValues: {
       query: '',
-      model: 'gpt-4-1106-preview',
+      model: 'gpt-4-0125-preview',
     },
   });
 
@@ -51,7 +51,7 @@ export default function AI({
     'gpt-3.5-turbo',
     'gpt-3.5-turbo-16k',
     'gpt-4',
-    'gpt-4-1106-preview',
+    'gpt-4-0125-preview',
   ];
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
--- app/types.ts
+++ app/types.ts
@@ -234,7 +234,7 @@ export type OpenAIModel =
   | 'gpt-3.5-turbo-16k'
   | 'gpt-3.5-turbo-1106'
   | 'gpt-4'
-  | 'gpt-4-1106-preview';
+  | 'gpt-4-0125-preview';
 
 export type AiQueryUsage = {
   promptTokens: number;
