e5860e1b249a44e674470d16295375d3314527fc
Ryan Quigley <ryan@auditre.co>
2024-02-11 13:17:30 -0500

    add TODO to unthread login codes

--- app/lib/email.ts
+++ app/lib/email.ts
@@ -26,6 +26,9 @@ async function sendEmail({
   subject: string;
   html: string;
 }) {
+  // TODO: add a header to prevent threading.
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/ses/command/SendRawEmailCommand/
+  // https://postmarkapp.com/support/article/1276-threading-unthreading-messages
   const command = new SendEmailCommand({
     Source: from,
     // ReplyToAddresses: [
