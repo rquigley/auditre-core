0a40f7518f118c5c8f5c8d7d1c95631a48336a94
Ryan Quigley <ryan@auditre.co>
2024-02-14 13:49:07 -0500

    fix query

--- app/controllers/org.ts
+++ app/controllers/org.ts
@@ -1,4 +1,4 @@
-import { db } from '@/lib/db';
+import { db, sql } from '@/lib/db';
 
 import type { NewOrg, OrgId, OrgUpdate } from '@/types';
 
@@ -31,18 +31,16 @@ export async function getOrgsWithMeta(ids: OrgId[]) {
   return await db
     .selectFrom('org as o')
     .leftJoin('auth.userRole as ur', 'o.id', 'ur.orgId')
-    .leftJoin('auth.user as u', 'ur.userId', 'u.id')
     .leftJoin('audit as a', 'o.id', 'a.orgId')
-    .select(({ fn }) => [
+    .select([
       'o.id',
       'o.name',
       'o.canHaveChildOrgs',
-      fn.count<number>('a.id').as('auditCount'),
-      fn.count<number>('u.id').as('userCount'),
+      sql<number>`COUNT(DISTINCT a.id)::int`.as('auditCount'),
+      sql<number>`COUNT(DISTINCT ur.org_id)::int`.as('userCount'),
     ])
     .where('o.id', 'in', ids)
     .where('o.isDeleted', '=', false)
-    .where('u.isDeleted', '=', false)
     .groupBy('o.id')
     .orderBy('name')
     .execute();
@@ -52,18 +50,16 @@ export async function getChildOrgsWithMeta(id: OrgId) {
   return await db
     .selectFrom('org as o')
     .leftJoin('auth.userRole as ur', 'o.id', 'ur.orgId')
-    .leftJoin('auth.user as u', 'ur.userId', 'u.id')
     .leftJoin('audit as a', 'o.id', 'a.orgId')
-    .select(({ fn }) => [
+    .select([
       'o.id',
       'o.name',
       'o.canHaveChildOrgs',
-      fn.count<number>('a.id').as('auditCount'),
-      fn.count<number>('u.id').as('userCount'),
+      sql<number>`COUNT(DISTINCT a.id)::int`.as('auditCount'),
+      sql<number>`COUNT(DISTINCT ur.org_id)::int`.as('userCount'),
     ])
     .where('o.parentOrgId', '=', id)
     .where('o.isDeleted', '=', false)
-    .where('u.isDeleted', '=', false)
     .groupBy('o.id')
     .orderBy('name')
     .execute();
