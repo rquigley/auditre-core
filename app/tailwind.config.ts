a6ad5294979dd8e698a45b5378aec7e5f6614e1d
Ryan Quigley <ryan@auditre.co>
2024-02-16 10:29:43 -0500

    don't look for pages in tailwind config.

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    // './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.tsx',
     './app/**/*.tsx',
     './controllers/output/react.tsx',
