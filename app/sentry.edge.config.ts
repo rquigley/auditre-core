f85c71c8630925f9a350cec9c0bd3b011af59a23
Ryan Quigley <ryan@auditre.co>
2024-03-11 13:49:21 -0400

    sentry cleanup

--- app/sentry.edge.config.ts
+++ app/sentry.edge.config.ts
@@ -10,7 +10,7 @@ Sentry.init({
   enabled: process.env.NODE_ENV !== 'development',
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
